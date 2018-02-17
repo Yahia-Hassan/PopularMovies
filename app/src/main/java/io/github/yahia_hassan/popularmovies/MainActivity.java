56de676c34a75cd721d8e6cae4d09059d4165b46
Yahia Hassan <yehia4747@gmail.com>
2018-02-17 15:08:44 +0200

    Add the Main Activity

--- app/src/main/java/io/github/yahia_hassan/popularmovies/MainActivity.java
+++ app/src/main/java/io/github/yahia_hassan/popularmovies/MainActivity.java
@@ -6,9 +6,13 @@ import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -19,6 +23,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final int LOADER_ID = 45;
+
+    private RecyclerView mRecyclerView;
+    private PopularMoviesAdapter mPopularMoviesAdapter;
+    private GridLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +66,37 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<String> loader, String data) {
-        Log.d(TAG, "JSON as String: " + data);
+        ArrayList<Movie> urlArrayList = new ArrayList<>();
+        urlArrayList.add(new Movie("/q0R4crx2SehcEEQEkYObktdeFy.jpg"));
+        urlArrayList.add(new Movie("/tWqifoYuwLETmmasnGHO7xBjEtt.jpg"));
+        urlArrayList.add(new Movie("/qey0tdcOp9kCDdEZuJ87yE3crSe.jpg"));
+        urlArrayList.add(new Movie("/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg"));
+        urlArrayList.add(new Movie("/xOfdQHNF9TlrdujyAjiKfUhxSXy.jpg"));
+        urlArrayList.add(new Movie("/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg"));
+        urlArrayList.add(new Movie("/coss7RgL0NH6g4fC2s5atvf3dFO.jpg"));
+        urlArrayList.add(new Movie("/AjHZIkzhPXrRNE4VSLVWx6dirK9.jpg"));
+        urlArrayList.add(new Movie("/47pLZ1gr63WaciDfHCpmoiXJlVr.jpg"));
+        urlArrayList.add(new Movie("/33IDooA29ctGHNE7gQW6cwbmRP3.jpg"));
+        urlArrayList.add(new Movie("/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg"));
+        urlArrayList.add(new Movie("/oDL2ryJ0sV2bmjgshVgJb3qzvwp.jpg"));
+        urlArrayList.add(new Movie("/5nvP4etJ8ecQv8qZM08dK4BAzdK.jpg"));
+        urlArrayList.add(new Movie("/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg"));
+        urlArrayList.add(new Movie("/6FxOPJ9Ysilpq0IgkrMJ7PubFhq.jpg"));
+        urlArrayList.add(new Movie("/doAzav9kfdtsoSdw1MDFvjKq3J4.jpg"));
+        urlArrayList.add(new Movie("/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg"));
+        urlArrayList.add(new Movie("/zMZTBbBa9eevwpWoVIVHh4Gm1ij.jpg"));
+        urlArrayList.add(new Movie("/9rtrRGeRnL0JKtu9IMBWsmlmmZz.jpg"));
+        urlArrayList.add(new Movie("/qmi2dsuoyzZdJ0WFZYQazbX8ILj.jpg"));
+
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mPopularMoviesAdapter = new PopularMoviesAdapter(this, urlArrayList);
+        mLayoutManager = new GridLayoutManager(this, 3);
+        Log.d(TAG, "Adapter and Layout attached");
+        mRecyclerView.setAdapter(mPopularMoviesAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
 
     }
 
---
+++ app/src/main/java/io/github/yahia_hassan/popularmovies/PopularMoviesAdapter.java
@@ -0,0 +1,59 @@
+package io.github.yahia_hassan.popularmovies;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class PopularMoviesAdapter extends RecyclerView.Adapter<PopularMoviesAdapter.PopularMoviesViewHolder> {
+
+    private ArrayList<Movie> mMovieArrayList;
+    private Context mContext;
+
+    public PopularMoviesAdapter (Context context, ArrayList<Movie> movieArrayList) {
+        mMovieArrayList = movieArrayList;
+        mContext = context;
+    }
+
+
+
+    @Override
+    public PopularMoviesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_item, parent, false);
+
+        return new PopularMoviesViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(PopularMoviesViewHolder holder, int position) {
+        Movie movie = mMovieArrayList.get(position);
+        String UrlFirstPart = "https://image.tmdb.org/t/p/w342/";
+
+        Picasso.with(mContext)
+                .load(UrlFirstPart + movie.getMoviePoster())
+                .into(holder.moviePoster);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovieArrayList.size();
+    }
+
+    public class PopularMoviesViewHolder extends RecyclerView.ViewHolder {
+        ImageView moviePoster;
+        public PopularMoviesViewHolder(View itemView) {
+            super(itemView);
+
+            moviePoster = itemView.findViewById(R.id.movie_poster);
+        }
+    }
+}
